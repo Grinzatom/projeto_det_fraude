a6bf165aefabd720dc1ace411ec7cf9e3dea19a3
Mauricio David Grinzato Sandoval <grinzatom@gmail.com>
2026-01-10 09:17:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ import numpy as np
 with open('modelo_fraude.pkl', 'rb') as file: 
     fraud_model = pickle.load(file)
 
-st.title("detector de Fraude em Transações")
+st.title("Detector de Fraude em Transações")
 st.write("Este modelo utiliza Regressão Logística para identificar possíveis fraudes em transações financeiras.")
 
-valor_transacao = st.number_input("Valor da Transação (em dólares):", min_value=0.0, max_value=100000.0, value=50.0)
+valor_transacao = st.number_input("Valor da Transação:", min_value=0.0, max_value=100000.0, value=50.0)
 tempo_conta = st.number_input("Tempo de Conta (em meses desde a criação da conta", min_value=0, max_value=120, value=6)
 num_transacoes_ult_30d = st.number_input("Número de Transações nos Últimos 30 Dias:", min_value=0, max_value=1000, value=3)
 
